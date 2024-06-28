8c80b765c1d836cacb50c82945f84a1d0dd3cc1c
nora <nora@norain.icu>
2024-06-28 05:32:45 -0700

    add loading screen and make scoring work

--- site/main.js
+++ site/main.js
@@ -1,11 +1,29 @@
-import Theme from "https://esm.sh/jsr/@iv/themes@0.0.1-alpha";
-import {
-  hex,
-  wcag,
-} from "https://esm.sh/jsr/@sondr3/a11y-color-contrast@0.2.0";
+"use strict";
+
+document.getElementById('content').style.display = 'none';
+
+const progress = document.getElementById('progress-bar');
+const title = progress.parentElement.getElementsByTagName('h1')[0];
+title.innerText = 'Loading themes framework';
+
+console.log('importing');
+
+const Theme = (await import("https://esm.sh/jsr/@iv/themes@0.0.1-alpha")).default;
+progress.style.width = '50%';
+title.innerText = 'Loading a11y tools'
+const { hex, wcag } = (await import("https://esm.sh/jsr/@sondr3/a11y-color-contrast@0.2.0"));
+progress.style.width = '50%';
+
+console.log('import finished');
+
+title.parentElement.style.opacity = 0;
+await new Promise((resolve) => {setInterval(resolve, 300)})
+title.parentElement.remove();
+document.getElementById('content').style.display = 'block';
 
 const theme = new Theme();
 theme.install();
+document.getElementById('theme-framework').remove();
 theme.start();
 
 const calculate = () => {
@@ -34,8 +52,10 @@ const calculate = () => {
 
   document.getElementById("pass").innerText = `${passed}/9`;
   document.getElementById("average").innerText = `${(scores / 9).toFixed(2)}`;
+  const score = passed / 9;
+  document.getElementById("score").innerText = `${(score * 100).toFixed(2)}% ${score > 0.9 ? 'perfect' : (score > 0.7 ? 'great' : (score > 0.5 ? 'good' : (score > 0.3 ? 'okay' : 'bad' )))}`;
 };
 
-calculate()
+calculate();
 
 theme.onLoad = calculate;
